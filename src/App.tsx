20b8c0c34acc4f9f847d4ed473750b56810a614b
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-13 18:16:25 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>PureStack Crypto Challenge 🚀</h1>
+      <p>Edit <code>src/App.tsx</code> to start building your dashboard.</p>
+      {/* TODO: Add your components here */}
+    </div>
+  )
+}
+
+export default App
