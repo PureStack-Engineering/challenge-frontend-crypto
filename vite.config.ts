500a091340db27d065297a95df00752f3e669a38
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-17 20:18:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,8 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.ts',
-  }
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', // <--- Conecta con tu archivo setup
+    css: true,
+  },
 })
