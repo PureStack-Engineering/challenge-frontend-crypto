d0b3a68d909f024f463b3ef6af68c7deabb15bcd
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2026-01-28 09:27:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts', // <--- Conecta con tu archivo setup
+    setupFiles: './src/setupTests.ts',
     css: true,
   },
 })
